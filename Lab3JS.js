f26ef4ae7fc6ef0f7e8f57209b4077eaba1bb5b0
Jakub Szpyrka <w67427@student.wsiz.edu.pl>
2024-04-21 09:22:10 +0200

    InitialCommit

---
+++ Lab3JS.js
@@ -0,0 +1,41 @@
+// alert("Hello World");
+
+// let a=10;
+// let b=20;
+// let c=23.2;
+
+//a
+// alert(a+b+c);
+//b
+// console.log(a+b+c);
+//c
+//let wynik = a+b+c;
+//document.getElementById("Zad3").innerHTML ="wynik dodawania to:" + wynik ;
+
+//Zad 4
+// let numberArray = [];
+// for (let i=0; i <= 100; i+=2){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+//Zad5  
+// let a = parseFloat(prompt("Podaj dlugosc boku1"));
+// let b = parseFloat(prompt("Podaj dlugosc boku2"));
+// let c = parseFloat(prompt("Podaj dlugosc boku3"));
+
+// let p = (a+b+c)/2;
+// let Pole =  Math.sqrt(p*(p-a)*(p-b)*(p-c));
+// console.log(Pole)
+//Zad 6
+
+// let imie = prompt("Podaj imie")
+// alert("Witaj"+ imie)
+
+//Zadanie 7
+let w = parseInt(prompt("Podaj pierwszą liczbę"));
+let e = parseInt(prompt("Podaj drugą liczbe"));
+
+let wynik = w+e;
+alert("wynik dodawania to" + " " + wynik)
