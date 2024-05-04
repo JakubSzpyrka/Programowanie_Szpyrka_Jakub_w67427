a940efeda722a134a908abececb1f31d92481161
Jakub Szpyrka <w67427@student.wsiz.edu.pl>
2024-05-04 15:53:31 +0200

    lab4

---
+++ script.js
@@ -0,0 +1,47 @@
+// Zadanie 1
+// a)
+// console.log("odliczanie sylwestrowe rozpoczyna się...");
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// console.log("szczęśliwego nowego roku!");
+
+// b)
+
+// console.log("odliczanie sylwestrowe rozpoczyna się...");
+// let countdown = 10;
+
+// while (countdown >= 0) {
+//     console.log(countdown);
+//     countdown--;
+// }
+
+// console.log("szczęśliwego nowego roku");
+
+// Zadanie2
+
+// function silnia(x){
+//     if (x==1 || x==0){
+//         return 1;
+//     }else{
+//         return x * silnia(x-1);
+//     }
+// }
+// console.log(silnia(5))
+//zadanie3
+
+// function sprawdz(x){
+//     let wiek = parseInt(prompt("Podaj wiek"));
+//     if (wiek==0) {
+//         return 0;
+//     }if (wiek>18){
+//         console.log("Jestes pelnoletni")
+//     }else{
+//         window.open("https://www.w3schools.com")
+//     }
+// }
+// sprawdz()
+// Zadanie 4
+
