6dc9937ba99f13a3adba9a67f9061adede1df604
JakubSzpyrka <kubaxus223@gmail.com>
2024-05-19 21:50:36 +0200

    Done

--- LAB4/script.js
+++ LAB4/script.js
@@ -1,47 +1,126 @@
 // Zadanie 1
-// a)
-// console.log("odliczanie sylwestrowe rozpoczyna się...");
-
-// for (let i = 10; i >= 0; i--) {
-//     console.log(i);
-// }
-
-// console.log("szczęśliwego nowego roku!");
-
-// b)
-
-// console.log("odliczanie sylwestrowe rozpoczyna się...");
-// let countdown = 10;
-
-// while (countdown >= 0) {
-//     console.log(countdown);
-//     countdown--;
-// }
-
-// console.log("szczęśliwego nowego roku");
-
-// Zadanie2
-
-// function silnia(x){
-//     if (x==1 || x==0){
+ 
+for(let i=1; i<=10; i++) {
+    console.log(i);
+}
+ 
+console.log("Happy New Year!");
+ 
+let i = 0;
+ 
+while (i <= 9) {
+    i++;
+    console.log(i);
+}
+console.log("Happy New Year!");
+ 
+// Zadanie 2
+ 
+// function silnia(n) {
+//     if (n > 1)
+//         return n * silnia(n-1);
+//     else
 //         return 1;
-//     }else{
-//         return x * silnia(x-1);
-//     }
+ 
 // }
-// console.log(silnia(5))
-//zadanie3
-
-// function sprawdz(x){
-//     let wiek = parseInt(prompt("Podaj wiek"));
-//     if (wiek==0) {
-//         return 0;
-//     }if (wiek>18){
-//         console.log("Jestes pelnoletni")
-//     }else{
-//         window.open("https://www.w3schools.com")
-//     }
+ 
+// let n = prompt("Podaj liczbę, dla której chcesz obliczyć slinię: ");
+// console.log(silnia(Number(n)));
+ 
+// Zadanie 3
+ 
+// function openWin() {
+//     let wiek = prompt("Podaj ile masz lat: ")
+//     if (wiek >= 18)
+//         window.open("", "_self")
+//     else
+//         window.open("https://www.gry.pl/")
 // }
-// sprawdz()
+ 
+// openWin();
+ 
 // Zadanie 4
-
+ 
+function changeColor(clickedButton) {
+    const buttons = document.querySelectorAll('.button');
+ 
+    buttons.forEach(button => {
+      if (button !== clickedButton) {
+        button.classList.remove('active');
+      }
+    });
+ 
+    clickedButton.classList.toggle('active');
+ 
+}
+ 
+// Zadanie 5
+ 
+function toggleVisibility() {
+  var element = document.getElementById('hiddenElement');
+  if (element.style.display === 'none') {
+      element.style.display = 'block';
+  } else {
+      element.style.display = 'none';
+  }
+}
+ 
+// Zadanie 6
+ 
+function addItem() {
+  var input = document.getElementById('itemInput');
+  var newItem = input.value;
+  if (newItem) {
+      var listItem = document.createElement('li');
+      listItem.textContent = newItem;
+      document.getElementById('itemList').appendChild(listItem);
+      input.value = '';
+  }
+}
+ 
+// Zadanie 7
+ 
+function addToTable() {
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  if (firstName && lastName) {
+      var table = document.getElementById('tableBody');
+      var row = table.insertRow();
+      var firstNameCell = row.insertCell(0);
+      var lastNameCell = row.insertCell(1);
+      firstNameCell.textContent = firstName;
+      lastNameCell.textContent = lastName;
+      document.getElementById('firstName').value = '';
+      document.getElementById('lastName').value = '';
+  }
+}
+ 
+// Zadanie 8
+ 
+function convertToFahrenheit() {
+  var celsius = document.getElementById('temperatureInput').value;
+  var fahrenheit = celsius * 9/5 + 32;
+  document.getElementById('result').textContent = `${fahrenheit} °F`;
+}
+ 
+function convertToCelsius() {
+  var fahrenheit = document.getElementById('temperatureInput').value;
+  var celsius = (fahrenheit - 32) * 5/9;
+  document.getElementById('result').textContent = `${celsius} °C`;
+}
+ 
+// Zadanie 10
+ 
+function calculate(operation) {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var result;
+  switch(operation) {
+      case '+': result = num1 + num2; break;
+      case '-': result = num1 - num2; break;
+      case '*': result = num1 * num2; break;
+      case '/': result = num1 / num2; break;
+      case '**': result = Math.pow(num1, num2); break;
+  }
+  document.getElementById('calcResult').textContent = "Wynik: " + result;
+}
--- LAB5/script.js
+++ LAB5/script.js
@@ -1,91 +1,79 @@
-// let tablica = []
-
-// for (let i=0; i<10;i++){
-//     tablica[i]=parseFloat(prompt("Podaj liczby"));
-// }
-// alert(tablica)
-
-// let liczba = parseFloat(prompt("podaj liczbe do wyszukania")) 
-// for(let x=0; x<10; x++){
-//     if (tablica[x] == liczba){
-//         console.log(liczba)
-//     }else{
-//         console.log("nie ma takiej loczby")
-//     }
-// }
-
-//Zadanie 2
-
-// let tablica = [1,2,3,4,5,6]
-// console.log(tablica)
-// let liczba = prompt("podaj liczbe na zamiane");
-// let gdzie = prompt("gdzie ja wstawic")
-
-// let dlugosc = tablica.length;
-// for(let i; i<dlugosc;i++){
-//     tablica.splice(gdzie, 0, liczba)
-//     console.log(tablica)
-// }
-
-
-// zadanie 3
-
-let tab = []
-for (let i=0; i<6; i++){
-    tab[i] = parseFloat(prompt("podaj liczby do ciagu"));
+// Zadanie 1
+ 
+let liczby = [];
+for (let i=0; i < 10; i++) {
+    let liczba = prompt("Wpisz liczbę całkowitą: ");
+    liczby.push(parseInt(liczba));
 }
-
-
-// zadanie 4
-
-
-//zadanie 5
-// Tablica liczb
-// let liczby = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-
-// let suma = liczby.reduce((acc, curr) => acc + curr, 0);
-// console.log("suma:", suma);
-
-
-// let parzyste = liczby.filter(num => num % 2 === 0);
-// console.log("parzyste", parzyste);
-
-
-// let mnozenie = liczby.map(element => element * 3);
-// console.log("mnozenie:", mnozenie);
-
-
-// let numerAlbumu = prompt("podaj nr albumu"); 
-// liczby.push(numerAlbumu);
-// let indexAlbumu = liczby.indexOf(numerAlbumu);
-// console.log("ndex numeru albumu:", indexAlbumu);
-
-
-// let srednia = suma / liczby.length;
-// console.log("srednia arytmetyczna:", srednia);
-
-
-// let max = Math.max(...liczby);
-// console.log("największa liczba:", max);
-
-
-// let wartosc = prompt("podaj wartosc"); 
-// let ile = liczby.filter(num => num === wartosc).length;
-// console.log("wystapienia", wartosc + ":", ile);
-
-//zadanie 6
-
-
-// function fibonaci(n) {
-//     let fibb = [0, 1];
-//     for (let i = 2; i < n; i++) {
-//         fibb[i] = fibb[i - 1] + fibb[i - 2];
-//     }
-//     return fibb;
-// }
-
-// let ciag = fibonaci(100);
-// console.log(ciag);
-
-//zadanie 7
+ 
+console.log("Wprowadzone liczby:", liczby);
+ 
+let szukanaLiczba = parseInt(prompt("Podaj liczbę całkowitą do znalezienia:"));
+let licznik = liczby.filter(x => x === szukanaLiczba).length;
+console.log(`Liczba ${szukanaLiczba} występuje ${licznik} razy w tablicy.`);
+ 
+// Zadanie 2
+ 
+let number = [1, 2, 3, 4, 5, 6];
+let indeks = parseInt(prompt("Podaj indeks, gdzie wstawić liczbę (0-5):"));
+let nowaLiczba = parseInt(prompt("Podaj nową liczbę do wstawienia:"));
+number.splice(indeks, 0, nowaLiczba);
+console.log("Zaktualizowana tablica:", number);
+ 
+// Zadanie 3
+ 
+let ciag = prompt("Wpisz ciąg znaków do odwrócenia:");
+let odwroconyCiąg = ciag.split('').reverse().join('');
+console.log("Odwrocony ciąg:", odwroconyCiąg);
+ 
+// Zadanie 5
+ 
+let numbers = [10, 20, 30, 40, 50];
+console.log("Suma:", numbers.reduce((a, b) => a + b, 0));
+console.log("Parzyste numbers:", numbers.filter(x => x % 2 === 0));
+console.log("Pomnożone wartości:", numbers.map(x => x * 3));
+numbers.push(69851);
+console.log("Index numeru albumu:", numbers.indexOf(69851));
+console.log("Średnia arytmetyczna:", numbers.reduce((a, b) => a + b, 0) / numbers.length);
+console.log("Największa liczba:", Math.max(...numbers));
+console.log("Ilość wystąpień wartości 20:", numbers.filter(x => x === 20).length);
+ 
+// Zadanie 6
+ 
+function generateFibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+  }
+  console.log(generateFibonacci(100));
+ 
+// Zadanie 7
+ 
+function sumOfTwoLargest(numbers) {
+    let sorted = numbers.sort((a, b) => b - a);
+    return sorted[0] + sorted[1];
+  }
+  let number1 = [10, 20, 30, 40, 50];
+  console.log("Suma dwóch największych liczb:", sumOfTwoLargest(number1));
+ 
+// Zadanie 8
+ 
+function removeDuplicates(array) {
+    return [...new Set(array)];
+  }
+  let number2 = [1, 2, 2, 3, 4, 5, 5, 6];
+  console.log("Tablica bez duplikatów:", removeDuplicates(number2));
+ 
+// Zadanie 9
+ 
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  }
+  let liczba = 17;
+  console.log(liczba + " jest liczbą pierwszą?", isPrime(liczba));
